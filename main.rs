93a433b9d5cd8f656f030a4705b023ef2fcc4ea9
김희수 <wlslaka4@gmail.com>
2024-03-16 17:17:42 +0900

    first rust

---
+++ main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let slice = "Hello!";
+    println!("Slice is {} bytes and also {} characters.", slice.len(), slice.chars().count());
+    let slice2 = "안녕!";
+    println!("Slice2 is {} bytes but only {} characters.", slice2.len(), slice2.chars().count());
+}
